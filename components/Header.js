2fabe6d0c4248654189b316a5a4a534b52a0ae34
JanMiskovic <janmiskovic13@gmail.com>
2022-09-11 19:32:48 +0200

    header wip

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
 import DarkToggle from "./DarkToggle";
 import LanguageSwitcher from "./LanguageSwitcher";
+import { FiStar } from "react-icons/fi";
 
 export default function Header() {
     return (
-        <header className="flex gap-3 justify-end">
-            <DarkToggle />
-            <LanguageSwitcher/>
+        <header className="flex justify-between items-center">
+            <a href="/" className="flex gap-2 items-center text-2xl underline underline-offset-4"><FiStar size={24}/>janmiskovic.com</a>
+            <div className="flex gap-3">
+                <DarkToggle />
+                <LanguageSwitcher/>
+            </div>
         </header>
     );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=optional"
+                    href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap"
                     rel="stylesheet"
                 />
             </Head>
