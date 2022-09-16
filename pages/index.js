082f15b77fdfe1e51864ae59cf4946567e985717
JanMiskovic <janmiskovic13@gmail.com>
2022-09-16 12:30:54 +0200

    portfolio wip 2

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
                 <title>Ján Miškovič</title>
             </Head>
             
-            <div className="grid grid-cols-2 gap-5">
+            <div className="grid grid-cols-2 gap-x-5 gap-y-3">
                 <a href="https://stavbytrnka.sk" target="_blank" rel="noreferrer"
                     className="flex flex-col">
                     <div className="my-border rounded-lg p-4 mb-1.5 bg-[#E2EEFF]">
@@ -30,6 +30,54 @@ export default function Home() {
                         />
                     </div>
                     <h3>Ervaar</h3>
+                    <span className="text-2xs">Wordpress (Elementor)</span>
+                </a>
+
+                <a href="https://addetailing.sk" target="_blank" rel="noreferrer"
+                    className="flex flex-col">
+                    <div className="my-border rounded-lg p-4 mb-1.5 bg-[#FFDEDE]">
+                        <Image src="/websites/addetailing.webp" width={640} height={360}
+                            alt="Preview image of website Stavby Trnka"
+                            className="rounded-sm drop-shadow-sharp"
+                        />
+                    </div>
+                    <h3>AD Detailing</h3>
+                    <span className="text-2xs">Wordpress (Elementor)</span>
+                </a>
+                
+                <a href="https://slezocoaching.sk" target="_blank" rel="noreferrer"
+                    className="flex flex-col">
+                    <div className="my-border rounded-lg p-4 mb-1.5 bg-[#F0F0FF]">
+                        <Image src="/websites/slezocoaching.webp" width={640} height={360}
+                            alt="Preview image of website Stavby Trnka"
+                            className="rounded-sm drop-shadow-sharp"
+                        />
+                    </div>
+                    <h3>Slezocoaching</h3>
+                    <span className="text-2xs">Wordpress (Elementor)</span>
+                </a>
+
+                <a href="https://linkvistica.com" target="_blank" rel="noreferrer"
+                    className="flex flex-col">
+                    <div className="my-border rounded-lg p-4 mb-1.5 bg-[#FFEFE0]">
+                        <Image src="/websites/linkvistica.webp" width={640} height={360}
+                            alt="Preview image of website Stavby Trnka"
+                            className="rounded-sm drop-shadow-sharp"
+                        />
+                    </div>
+                    <h3>Linkvistica</h3>
+                    <span className="text-2xs">Wordpress (Elementor)</span>
+                </a>
+
+                <a href="https://majdovan.com" target="_blank" rel="noreferrer"
+                    className="flex flex-col">
+                    <div className="my-border rounded-lg p-4 mb-1.5 bg-[#E0EDFF]">
+                        <Image src="/websites/majdovan.webp" width={640} height={360}
+                            alt="Preview image of website Stavby Trnka"
+                            className="rounded-sm drop-shadow-sharp"
+                        />
+                    </div>
+                    <h3>Majdovan</h3>
                     <span className="text-2xs">Wordpress (Elementor)</span>
                 </a>
             </div>
