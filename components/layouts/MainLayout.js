160be2c82407cc568bc14d2c3c2747c387f9dfa8
JanMiskovic <jan@janmiskovic.com>
2022-10-27 17:16:38 +0200

    fade in delay test

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -20,7 +20,7 @@ export default function MainLayout({ children }) {
 
     const initialLoadVariants = {
         initial: { opacity: 0 },
-        animate: { opacity: 1 },
+        animate: { opacity: 1, transition: { delay: 0.15 } },
     };
 
     const pageTransitionVariants = {
