1df5a8c58104878e32ae13a4aadb1672e4bb23be
JanMiskovic <jan@janmiskovic.com>
2022-10-30 16:06:16 +0100

    noindex dev deploys

--- next.config.js
+++ next.config.js
@@ -5,6 +5,22 @@ const nextConfig = {
         locales: ["en", "sk"],
         defaultLocale: "en",
     },
+
+    async headers() {
+        const headers = [];
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+            headers.push({
+                headers: [
+                    {
+                        key: "X-Robots-Tag",
+                        value: "noindex",
+                    },
+                ],
+                source: "/:path*",
+            });
+        }
+        return headers;
+    },
 };
 
 module.exports = nextConfig;
