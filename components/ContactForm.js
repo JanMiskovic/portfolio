816abdbd0423885032544f1ee0b993c9492d72cb
JanMiskovic <jan@janmiskovic.com>
2022-11-09 20:12:06 +0100

    form wip

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,6 +1,6 @@
 import { FormattedMessage, useIntl } from "react-intl";
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { IoSend } from "react-icons/io5";
 import { VscDebugRestart } from "react-icons/vsc";
 import TextareaAutosize from "react-textarea-autosize";
@@ -40,142 +40,157 @@ export default function ContactForm() {
 
         const { error } = await res.json();
 
-        if (error) {
-            setSendStatus("failure");
-        } else {
-            setSendStatus("success");
-            resetFields();
-        }
+        setSendStatus(error ? "failure" : "success");
+        setTimeout(() => setSendStatus(""), 5000);
     };
 
     return (
-        <form onSubmit={handleSubmit} className="grid gap-4">
-            <input
-                required
-                maxLength={30}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder={intl.formatMessage({
-                    id: "contact.form.name",
-                })}
-                type="text"
-                name="name"
-                className="my-border rounded-md bg-body-light px-3 py-2.5
-                placeholder-neutral-700 focus:outline-none
-                dark:bg-[#1f1f1f] dark:placeholder-neutral-300
-                lg:col-start-1 lg:py-3"
-            />
-            <input
-                maxLength={60}
-                value={email}
-                onChange={(e) => setEmail(() => e.target.value)}
-                placeholder={intl.formatMessage({
-                    id: "contact.form.email",
-                })}
-                type="email"
-                name="email"
-                className="my-border rounded-md bg-body-light px-3 py-2.5
-                placeholder-neutral-700 focus:outline-none
-                dark:bg-[#1f1f1f] dark:placeholder-neutral-300
-                lg:col-start-2 lg:py-3"
-            />
-            <input
-                required
-                maxLength={100}
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-                placeholder={intl.formatMessage({ id: "contact.form.subject" })}
-                type="text"
-                name="subject"
-                className="my-border rounded-md bg-body-light px-3 py-2.5
-                placeholder-neutral-700 focus:outline-none
-                dark:bg-[#1f1f1f] dark:placeholder-neutral-300
-                lg:col-span-2 lg:py-3"
-            />
-            <div className="relative flex lg:col-span-2">
-                <TextareaAutosize
+        <div>
+            <form onSubmit={handleSubmit} className="grid gap-4">
+                <input
                     required
-                    maxLength={2000}
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
+                    maxLength={30}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     placeholder={intl.formatMessage({
-                        id: "contact.form.text",
+                        id: "contact.form.name",
                     })}
-                    name="message"
-                    className="my-border min-h-[10.625rem] w-full
-                    resize-none overflow-hidden rounded-md bg-body-light px-3 pt-2.5 pb-14
+                    type="text"
+                    name="name"
+                    className="my-border rounded-md bg-body-light px-3 py-2.5
                     placeholder-neutral-700 focus:outline-none
                     dark:bg-[#1f1f1f] dark:placeholder-neutral-300
-                    xs:pt-3 xs:pb-16"
+                    lg:col-start-1 lg:py-3"
                 />
-                <div className="absolute bottom-4 right-4 flex gap-2 xs:gap-2.5">
-                    <div className="relative">
+                <input
+                    maxLength={60}
+                    value={email}
+                    onChange={(e) => setEmail(() => e.target.value)}
+                    placeholder={intl.formatMessage({
+                        id: "contact.form.email",
+                    })}
+                    type="email"
+                    name="email"
+                    className="my-border rounded-md bg-body-light px-3 py-2.5
+                    placeholder-neutral-700 focus:outline-none
+                    dark:bg-[#1f1f1f] dark:placeholder-neutral-300
+                    lg:col-start-2 lg:py-3"
+                />
+                <input
+                    required
+                    maxLength={100}
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                    placeholder={intl.formatMessage({
+                        id: "contact.form.subject",
+                    })}
+                    type="text"
+                    name="subject"
+                    className="my-border rounded-md bg-body-light px-3 py-2.5
+                    placeholder-neutral-700 focus:outline-none
+                    dark:bg-[#1f1f1f] dark:placeholder-neutral-300
+                    lg:col-span-2 lg:py-3"
+                />
+                <div className="relative flex lg:col-span-2">
+                    <TextareaAutosize
+                        required
+                        maxLength={2000}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder={intl.formatMessage({
+                            id: "contact.form.text",
+                        })}
+                        name="message"
+                        className="my-border min-h-[10.625rem] w-full
+                        resize-none overflow-hidden rounded-md bg-body-light px-3 pt-2.5 pb-14
+                        placeholder-neutral-700 focus:outline-none
+                        dark:bg-[#1f1f1f] dark:placeholder-neutral-300
+                        xs:pt-3 xs:pb-16"
+                    />
+                    <div className="absolute bottom-4 right-4 flex gap-2 xs:gap-2.5">
+                        <div className="relative">
+                            <button
+                                type="button"
+                                onClick={() => setShowResetConfirm(true)}
+                                className="focus-ring my-border transition-hover h-full rounded-md
+                                bg-[#E9F7FF] py-1.5 px-2 text-center text-base
+                                hover:bg-[#dbf2ff] active:bg-[#ccecff]
+                                dark:bg-[#66C8FF] dark:hover:bg-[#85d2ff] dark:active:bg-[#a3ddff]
+                                xs:text-xl sm:py-2 sm:px-2.5">
+                                <VscDebugRestart className="fill-icon-light dark:fill-icon-dark" />
+                            </button>
+                            {showResetConfirm && (
+                                <div
+                                    className="my-border absolute left-1/2 mt-3 flex -translate-x-1/2
+                                    gap-3 rounded-md bg-bg-light py-3 px-4 dark:bg-bg-dark">
+                                    <button
+                                        type="button"
+                                        onClick={() =>
+                                            setShowResetConfirm(false)
+                                        }>
+                                        No
+                                    </button>
+                                    <button
+                                        type="reset"
+                                        onClick={() => {
+                                            setShowResetConfirm(false);
+                                            resetFields();
+                                        }}>
+                                        Yes
+                                    </button>
+                                </div>
+                            )}
+                        </div>
                         <button
-                            type="button"
-                            onClick={() => setShowResetConfirm(true)}
-                            className="focus-ring my-border transition-hover h-full rounded-md
-                            bg-[#E9F7FF] py-1.5 px-2 text-center text-base
-                            hover:bg-[#dbf2ff] active:bg-[#ccecff]
+                            type="submit"
+                            disabled={sendStatus === "sending" ? true : false}
+                            className="focus-ring my-border transition-hover
+                            flex w-[6.5rem] items-center justify-center gap-2 rounded-md
+                            bg-[#E9F7FF] py-1.5 text-center text-sm
+                            text-black hover:bg-[#dbf2ff] active:bg-[#ccecff]
                             dark:bg-[#66C8FF] dark:hover:bg-[#85d2ff] dark:active:bg-[#a3ddff]
-                            xs:text-xl sm:py-2 sm:px-2.5">
-                            <VscDebugRestart className="fill-icon-light dark:fill-icon-dark" />
+                            xs:w-32 xs:text-base sm:w-36 sm:py-2">
+                            <FormattedMessage id="contact.form.send" />{" "}
+                            <IoSend className="fill-icon-light dark:fill-icon-dark" />
                         </button>
-                        {showResetConfirm && (
-                            <div
-                                className="my-border absolute left-1/2 mt-3 flex -translate-x-1/2
-                                gap-3 rounded-md bg-bg-light py-3 px-4 dark:bg-bg-dark">
-                                <button
-                                    type="button"
-                                    onClick={() => setShowResetConfirm(false)}>
-                                    No
-                                </button>
-                                <button
-                                    type="reset"
-                                    onClick={() => {
-                                        setShowResetConfirm(false);
-                                        resetFields();
-                                    }}>
-                                    Yes
-                                </button>
-                            </div>
-                        )}
                     </div>
-                    <button
-                        type="submit"
-                        className="focus-ring my-border transition-hover
-                        flex w-[6.5rem] items-center justify-center gap-2 rounded-md
-                        bg-[#E9F7FF] py-1.5 text-center text-sm
-                        text-black hover:bg-[#dbf2ff] active:bg-[#ccecff]
-                        dark:bg-[#66C8FF] dark:hover:bg-[#85d2ff] dark:active:bg-[#a3ddff]
-                        xs:w-32 xs:text-base sm:w-36 sm:py-2">
-                        <FormattedMessage id="contact.form.send" />{" "}
-                        <IoSend className="fill-icon-light dark:fill-icon-dark" />
-                    </button>
                 </div>
-            </div>
+            </form>
 
-            {sendStatus === "success" && (
-                <p className="text-white">
-                    <span className="inline-block">
-                        Vaša správa bola úspešne odoslaná!&nbsp;
-                    </span>
-                    <span className="inline-block">
-                        Ozveme sa Vám čo najskôr.
-                    </span>
-                </p>
-            )}
-            {sendStatus === "failure" && (
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    className="">
-                    <p className="text-white">
-                        <span className="inline-block">
-                            Vyskytla sa chyba :/&nbsp;
-                        </span>
-                    </p>
-                </motion.div>
-            )}
-        </form>
+            <AnimatePresence>
+                {sendStatus === "success" || sendStatus === "failure" && (
+                    <motion.div
+                        variants={resultVariants}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit">
+                        <p className="pt-4">
+                            Vyskytla sa chyba :/ Kontaktujte ma prosím priamo
+                            cez e-mail.
+                        </p>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
     );
 }
+
+const resultVariants = {
+    initial: { opacity: 0, height: 0 },
+    animate: {
+        opacity: 1,
+        height: "auto",
+        transition: {
+            opacity: { delay: 0.05, duration: 0.2 },
+            height: { ease: "easeOut", duration: 0.2 },
+        },
+    },
+    exit: {
+        opacity: 0,
+        height: 0,
+        transition: {
+            opacity: { duration: 0.2 },
+            height: { delay: 0.08, ease: "easeOut", duration: 0.2 },
+        },
+    },
+};
--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -3,45 +3,46 @@ import sendgrid from "@sendgrid/mail";
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function sendEmail(req, res) {
-    try {
-        await sendgrid.send({
-            to: "stavbytrnkaformular@gmail.com",
-            from: {email: "formular@stavbytrnka.sk", name: req.body.name},
-            subject: `Formulár: ${req.body.subject}`,
-            replyTo: {email: req.body.email, name: req.body.name},
-            html: 
-            `<!DOCTYPE html>
-            <html lang="sk">
+    // try {
+    //     await sendgrid.send({
+    //         to: "jan@janmiskovic.com",
+    //         from: {email: "formular@janmiskovic.com", name: req.body.name},
+    //         subject: `Formulár: ${req.body.subject}`,
+    //         replyTo: {email: req.body.email, name: req.body.name},
+    //         html: 
+    //         `<!DOCTYPE html>
+    //         <html lang="sk">
             
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Formulár od ${req.body.name}</title>
-            </head>
+    //         <head>
+    //             <meta charset="UTF-8">
+    //             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    //             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //             <title>Formulár od ${req.body.name}</title>
+    //         </head>
             
-            <body style="margin: 0; padding-top: 0.5em; padding-bottom: 0.5em; font-family: Arial, Helvetica, sans-serif; max-width: 500px;">
-                <h2 style="margin: 0; padding: 0; font-size: 3em; line-height: 1em;">
-                    STAVBY Trnka s.r.o.
-                </h2>
+    //         <body style="margin: 0; padding-top: 0.5em; padding-bottom: 0.5em; font-family: Arial, Helvetica, sans-serif; max-width: 500px;">
+    //             <h2 style="margin: 0; padding: 0; font-size: 3em; line-height: 1em;">
+    //                 STAVBY Trnka s.r.o.
+    //             </h2>
             
-                <h3>Nová správa z formuláru:</h3>
-                <hr />
+    //             <h3>Nová správa z formuláru:</h3>
+    //             <hr />
             
-                <ul style="list-style: none; margin: 0; padding: 0;">
-                    <li style="margin-left: 0; padding-top: 0.5em;"><strong>Meno: </strong>${req.body.name}</li>
-                    <li style="margin-left: 0; padding-top: 0.5em;"><strong>E-mail: </strong>${req.body.email}</li>
-                    <li style="margin-left: 0; padding-top: 0.5em;"><strong>Predmet: </strong>${req.body.subject}</li>
-                    <li style="margin-left: 0; padding-top: 1em"><strong>Správa: </strong></li>
-                    <li style="margin-left: 0; padding-top: 1em;">${req.body.message}</li>
-                </ul>
-            </body>
+    //             <ul style="list-style: none; margin: 0; padding: 0;">
+    //                 <li style="margin-left: 0; padding-top: 0.5em;"><strong>Meno: </strong>${req.body.name}</li>
+    //                 <li style="margin-left: 0; padding-top: 0.5em;"><strong>E-mail: </strong>${req.body.email}</li>
+    //                 <li style="margin-left: 0; padding-top: 0.5em;"><strong>Predmet: </strong>${req.body.subject}</li>
+    //                 <li style="margin-left: 0; padding-top: 1em"><strong>Správa: </strong></li>
+    //                 <li style="margin-left: 0; padding-top: 1em;">${req.body.message}</li>
+    //             </ul>
+    //         </body>
             
-            </html>`,
-        });
-    } catch (error) {
-        return res.status(error.statusCode || 500).json({ error: error.message });
-    }
+    //         </html>`,
+    //     });
+    // } catch (error) {
+    //     return res.status(error.statusCode || 500).json({ error: error.message });
+    // }
 
-    return res.status(200).json({ error: "" });
+    // return res.status(200).json({ error: "" });¨
+    return res.json({ error: "Disabled" });
 }
