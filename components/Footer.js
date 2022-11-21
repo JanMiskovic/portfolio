99b14314aa14ab9176d654613041cdcb34262c96
JanMiskovic <jan@janmiskovic.com>
2022-11-21 12:43:49 +0100

    add source to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,4 @@
+import { BsGithub } from "react-icons/bs";
 import { FaCookieBite } from "react-icons/fa";
 import { FormattedMessage } from "react-intl";
 
@@ -5,19 +6,29 @@ export default function Footer({ setShowCookieBanner }) {
     return (
         <footer
             className="mt-6 flex justify-between border-t border-neutral-400 pt-3
-            text-sm text-neutral-500 dark:border-neutral-500 dark:text-neutral-400 sm:mt-7 sm:border-t-0">
+            text-sm text-neutral-500 dark:border-neutral-500 dark:text-neutral-400
+            sm:mt-7 sm:border-t-0">
             <button
                 onClick={() => {
                     setShowCookieBanner(true);
                     window.gtag("event", "manage_cookies");
                 }}
-                className="focus-ring focus-ring-loose transition-hover flex items-center gap-1.5
-                rounded-md hover:text-neutral-600 active:text-neutral-600
+                className="focus-ring focus-ring-loose transition-hover flex items-center 
+                gap-1.5 rounded-md hover:text-neutral-600 active:text-neutral-600
                 hover:dark:text-neutral-300 active:dark:text-neutral-300">
-                <FormattedMessage id="footer.manage.cookies" />
+                <FormattedMessage id="footer.cookies" />
                 <FaCookieBite />
             </button>
-            <span className="">© {new Date().getFullYear()} Ján Miškovič</span>
+            <a
+                href="https://github.com/JanMiskovic/portfolio"
+                target="_blank"
+                rel="noreferrer"
+                className="focus-ring focus-ring-loose transition-hover flex items-center
+                gap-1.5 rounded-md hover:text-neutral-600 active:text-neutral-600
+                hover:dark:text-neutral-300 active:dark:text-neutral-300">
+                <BsGithub />
+                <FormattedMessage id="footer.source" />
+            </a>
         </footer>
     );
 }
